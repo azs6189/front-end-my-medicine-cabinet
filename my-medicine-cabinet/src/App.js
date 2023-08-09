7c5fcf3062e8eece7bf7b87368eb9ddf7ec37b41
Allie Soliz <solizale@gmail.com>
2023-08-09 15:45:31 -0400

    imports MedicationInfo component

--- my-medicine-cabinet/src/App.js
+++ my-medicine-cabinet/src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import MedicationInfo from './components/MedicationLog';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div id="App">
+			<header>
+				<h1>My Medicine Cabinet</h1>
+			</header>
+
+			<main>
+				<MedicationInfo></MedicationInfo>
+			</main>
+		</div>
+	);
 }
 
 export default App;
