2140200ba14cd48d9f7b9060b7134db13ca61bdf
Allie Soliz <solizale@gmail.com>
2023-08-12 23:42:37 -0400

    ensures text input field's value comes from the component's state

--- my-medicine-cabinet/src/components/MedicationNameInput.js
+++ my-medicine-cabinet/src/components/MedicationNameInput.js
@@ -1,16 +1,31 @@
 import React from 'react';
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
-const SearchBar = () => {
+const MedicationNameInput = () => {
+	const [medicationName, setMedicationName] = useState('Eliquis');
+
+	const renameMedication = (changeEvent) => {
+		console.log('Details about the element that fired the event:', changeEvent);
+		console.log('The value of that element:', changeEvent.target.value);
+		setMedicationName(changeEvent.target.value);
+	};
+
 	return (
-		<div className="form-group fg--search">
-			<input type="text" className="input" placeholder="Search medication" />
-			<button type="submit">
-				<FontAwesomeIcon icon={faMagnifyingGlass} />
-			</button>
-		</div>
+		<form className="form-group fg--search">
+			<input
+				type="text"
+				className="input"
+				value={medicationName}
+				onChange={renameMedication}
+			/>
+
+			<input type="submit" value="Submit"></input>
+
+			<FontAwesomeIcon icon={faMagnifyingGlass} />
+		</form>
 	);
 };
 
-export default SearchBar;
+export default MedicationNameInput;
